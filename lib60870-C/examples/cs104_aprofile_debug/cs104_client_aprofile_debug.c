7bd7a2f7cb0b40619e653dbc611cb23cb037f3cf
shyamarshid <shyamkumar853@gmail.com>
2025-11-27 17:52:38 +0530

    Enable secure ALS flows in A-profile debug examples

--- lib60870-C/examples/cs104_aprofile_debug/cs104_client_aprofile_debug.c
+++ lib60870-C/examples/cs104_aprofile_debug/cs104_client_aprofile_debug.c
@@ -38,6 +38,56 @@ print_hex(const uint8_t* b, int len)
         printf("%02X ", b[i]);
 }
 
+static const uint8_t CLIENT_OUTBOUND_SESSION_KEY[APROFILE_SESSION_KEY_LENGTH] = {
+    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+    0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10,
+    0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18,
+    0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20
+};
+
+static const uint8_t CLIENT_INBOUND_SESSION_KEY[APROFILE_SESSION_KEY_LENGTH] = {
+    0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28,
+    0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F, 0x30,
+    0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38,
+    0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F, 0x40
+};
+
+static const uint8_t UPDATE_AUTH_KEY[APROFILE_SESSION_KEY_LENGTH] = {
+    0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7,
+    0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF,
+    0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6, 0xB7,
+    0xB8, 0xB9, 0xBA, 0xBB, 0xBC, 0xBD, 0xBE, 0xBF
+};
+
+static const uint8_t UPDATE_ENC_KEY[APROFILE_SESSION_KEY_LENGTH] = {
+    0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7,
+    0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF,
+    0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7,
+    0xD8, 0xD9, 0xDA, 0xDB, 0xDC, 0xDD, 0xDE, 0xDF
+};
+
+static void
+configureSecurity(CS104_SecurityConfig* sec)
+{
+    memset(sec, 0, sizeof(*sec));
+
+    sec->aim = 0x1001;
+    sec->ais = 0x2001;
+#ifdef APROFILE_DPA_HMAC_SHA256_TCP
+    sec->dpaAlgorithm = APROFILE_DPA_HMAC_SHA256_TCP;
+#else
+    sec->dpaAlgorithm = APROFILE_DPA_HMAC_SHA256;
+#endif
+
+    sec->hasStaticSessionKeys = true;
+    memcpy(sec->outboundSessionKey, CLIENT_OUTBOUND_SESSION_KEY, sizeof(sec->outboundSessionKey));
+    memcpy(sec->inboundSessionKey, CLIENT_INBOUND_SESSION_KEY, sizeof(sec->inboundSessionKey));
+
+    sec->hasUpdateKeys = true;
+    memcpy(sec->authenticationUpdateKey, UPDATE_AUTH_KEY, sizeof(sec->authenticationUpdateKey));
+    memcpy(sec->encryptionUpdateKey, UPDATE_ENC_KEY, sizeof(sec->encryptionUpdateKey));
+}
+
 static void
 handleControlTag(uint8_t tag, const CS104_SecurityConfig* sec)
 {
@@ -163,17 +213,11 @@ main(int argc, char** argv)
 
     CS104_Connection conn = CS104_Connection_create(ip, port);
 
-    CS104_SecurityConfig sec = {0};
-    sec.aim = 0x1001;
-    sec.ais = 0x2001;
-#ifdef APROFILE_DPA_HMAC_SHA256_TCP
-    sec.dpaAlgorithm = APROFILE_DPA_HMAC_SHA256_TCP;
-#else
-    sec.dpaAlgorithm = APROFILE_DPA_HMAC_SHA256;
-#endif
+    CS104_SecurityConfig sec;
+    configureSecurity(&sec);
 
     CS104_CertConfig cert = { .localCertificateVerified = true, .peerCertificateVerified = true };
-    CS104_RoleConfig role = { .rolesAvailable = false };
+    CS104_RoleConfig role = { .rolesAvailable = true };
 
     CS104_Connection_setSecurityConfig(conn, &sec, &cert, &role);
 
